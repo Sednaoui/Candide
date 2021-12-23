ff146cc75cb1d334fb390607ab7a407f33690285
Marc Sednaoui <marcsednaoui@hotmail.com>
2021-12-23 10:47:54 +0100

    lint eslinttr.js (lol)

--- crosscash-extension/.eslintrc.js
+++ crosscash-extension/.eslintrc.js
@@ -1,40 +1,40 @@
 module.exports = {
-    extends: ['eslint-config-willo-react', "eslint-config-willo-typescript"],
+    extends: ['eslint-config-willo-react', 'eslint-config-willo-typescript'],
     rules: {
         'react/jsx-uses-react': 'off',
         'react/react-in-jsx-scope': 'off',
-        "no-use-before-define": "off",
-        "import/extensions": [
-            "error",
-            "ignorePackages",
+        'no-use-before-define': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
             {
-                "js": "never",
-                "jsx": "never",
-                "ts": "never",
-                "tsx": "never"
-            }
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
         ],
-        "no-unused-vars": "off",
+        'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': [
-          'warn',
-          {
-            'argsIgnorePattern': '^_',
-            'varsIgnorePattern': '^_',
-            'caughtErrorsIgnorePattern': '^_'
-          }
-        ]
+            'warn',
+            {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+                caughtErrorsIgnorePattern: '^_',
+            },
+        ],
     },
     settings: {
-        "import/resolver": {
-            "node": {
-                "extensions": [".js", ".jsx", ".ts", ".tsx"]
-            }
-        }
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
     },
     env: {
-        "jest": true,
-        "es6": true,
-        "node": true,
-        "browser": true
-    }
+        jest: true,
+        es6: true,
+        node: true,
+        browser: true,
+    },
 };
