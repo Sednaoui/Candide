0455f5af55d6a21aab0f73ce87e1c0d3d831f117
Marc Sednaoui <marcsednaoui@hotmail.com>
2021-12-25 21:26:47 +0100

    Create login pages and a basic wallet page with  auth routing

--- crosscash-extension/src/popup/App.tsx
+++ crosscash-extension/src/popup/App.tsx
@@ -1,27 +1,35 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import logo from '../logo.svg';
 import './App.css';
+import { useAuth } from './pages/auth/AuthProvider';
+import { RootState } from './store/rootReducer';
 
 function App(): JSX.Element {
+    const auth = useAuth();
+    const { walletInstance } = useSelector((state: RootState) => state.wallet);
+
+    let initialPath = '/';
+
+    if (walletInstance?.address && auth.user) {
+        initialPath = '/wallet';
+    } else if (walletInstance?.address && !auth.user) {
+        initialPath = '/login';
+    } else {
+        initialPath = '/import_wallet';
+    }
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit
-                    {' '}
-                    <code>
-                        src/App.js
-                    </code>
-                    {' '}
-                    and save to reload.
+                    Welcome to Crosscash
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
+                <Link to={initialPath}>
+                    {initialPath === '/login' ? 'Login' : 'Get Started'}
+                </Link>
             </header>
         </div>
     );
