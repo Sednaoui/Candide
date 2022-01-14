bfdd5da9f7fe3f7d8ccae60e3534888751e16458
Marc Sednaoui <marcsednaoui@hotmail.com>
2022-01-14 12:37:54 +0100

    add comment to trancataddress function

--- crosscash-extension/src/util/helpers.ts
+++ crosscash-extension/src/util/helpers.ts
@@ -4,6 +4,10 @@ import {
     ROPSTEN,
 } from './constants/networks';
 import { EVMNetwork } from './networks';
+
+/**
+ * trancats an address to 8 bytes, and pads the rest with four dots
+ */
 export const trancatAddress = (address: string): string => (
     `${address.substring(0, 4)}...${address.substring(address.length - 4)}`
 );
