a24c3bff12c790b4ec0e2517051b537fa03adb6e
Marc Sednaoui <marcsednaoui@hotmail.com>
2022-01-28 12:41:16 +0100

    remove ETH_PROVIDER constant since it's unused

--- crosscash-extension/src/popup/model/constants.ts
+++ crosscash-extension/src/popup/model/constants.ts
@@ -2,4 +2,3 @@
 export const CURRENT_NETWORK = 'ropsten';
 export const INFURA_ID = '';
 export const ALCHEMY_API_KEY = '';
-export const ETH_PROVIDER = `https://${CURRENT_NETWORK}.infura.io/v3/${INFURA_ID}`;
