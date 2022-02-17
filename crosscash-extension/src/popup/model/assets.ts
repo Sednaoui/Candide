96e22e3acccbf802cc1d34c95e6f7e88a1cc06be
Marc Sednaoui <marcsednaoui@hotmail.com>
2022-02-17 21:07:42 +0100

    modify asset function retrieveTokenBalances; replacing alchemy function with zapper

--- crosscash-extension/src/popup/model/assets.ts
+++ crosscash-extension/src/popup/model/assets.ts
@@ -1,13 +1,15 @@
 import { AlchemyProvider } from '@ethersproject/providers';
 
 import { HexString } from '../../lib/accounts';
-import {
-    getTokenBalances,
-    getTokenMetadata,
-} from '../../lib/alchemy';
+import { getTokenMetadata } from '../../lib/alchemy';
 import { AnyAssetAmount } from '../../lib/assets';
-import { ETH } from '../../lib/constants/currencies';
-import { fromFixedPoint } from '../../lib/helpers';
+import {
+    baseAddress,
+    ETH,
+    MATIC,
+} from '../../lib/constants/currencies';
+import { POLYGON } from '../../lib/constants/networks';
+import { getBalances } from '../../lib/zapper';
 
 /**
  * Retrieve token balances for a particular account on a particular network,
@@ -22,46 +24,40 @@ export async function retrieveTokenBalances(
     provider: AlchemyProvider,
     address: HexString,
 ): Promise<AnyAssetAmount[]> {
-    // get ERC-20 balances
-    const balances = await getTokenBalances(
-        provider,
+    // get base asset balance and ERC-20 balances
+    const balances = await getBalances(
         address,
+        provider.network.chainId,
     );
 
-    // get ETH balance
-    const ethBalance = await provider.getBalance(address);
+    // get base asset balance
+    const baseAssetBalance = balances.find(({ contractAddress }) =>
+        contractAddress === baseAddress)?.amount || 0;
 
-    const formatedEthBalance = fromFixedPoint(
-        BigInt(ethBalance.toString()),
-        ETH.decimals,
-        4,
-    );
-
-    const ethAssetAmount = {
-        asset: ETH,
-        amount: formatedEthBalance,
+    const baseAssetAmount = {
+        asset: provider.network.chainId === Number(POLYGON.chainID) ? MATIC : ETH,
+        amount: baseAssetBalance,
     };
 
-    const nonZeroBalances = balances.filter((balance) => balance.amount > 0n);
+    // filter out non-zero balances && base asset balance
+    const nonZeroERC20Balances = balances.filter(
+        (balance) => balance.amount > 0n && balance.contractAddress !== baseAddress,
+    );
 
-    const erc20AssetAmount = Promise.all(nonZeroBalances.map(async ({
+    const erc20AssetAmount = Promise.all(nonZeroERC20Balances.map(async ({
         contractAddress,
         amount,
     }) => {
         const token = await getTokenMetadata(provider, contractAddress);
 
-        const { decimals } = token;
-
-        const balanceAmount = fromFixedPoint(amount, decimals, 4);
-
         return {
             asset: token,
-            amount: balanceAmount,
+            amount,
         };
     }));
 
     return [
-        ethAssetAmount,
+        baseAssetAmount,
         ...(await erc20AssetAmount),
     ];
 }
