62d9b5c24560a9d2837284eb7239a044a71df04b
Marc Sednaoui <marcsednaoui@hotmail.com>
2022-02-17 21:10:12 +0100

    remove alchemy prev used functions to get token balances for an account

--- crosscash-extension/src/lib/alchemy.ts
+++ crosscash-extension/src/lib/alchemy.ts
@@ -14,23 +14,6 @@ import { ETH } from './constants/currencies';
 import { getEthereumNetwork } from './helpers';
 
 /**
- * alchemy_getTokenBalances return type for tokenBalances
- */
-type TokenBalance = {
-    contractAddress: HexString;
-    tokenBalance: string;
-    error: string;
-}
-
-/**
- * alchemy_getTokenBalances return type for token Balances
- */
-type TokenBalances = {
-    address: HexString;
-    tokenBalances: TokenBalance[];
-}
-
-/**
  * alchemy_getTokenMetadata return type
  */
 type TokenMetaData = {
@@ -38,59 +21,6 @@ type TokenMetaData = {
     symbol: string;
     decimals: number;
     logo: string;
-}
-
-/**
- * Use Alchemy's getTokenBalances call to get balances for a particular address.
- *
- *
- * More information https://docs.alchemy.com/alchemy/documentation/enhanced-apis/token-api
- *
- * @param provider an Alchemy ethers provider
- * @param address the address whose balances we're fetching
- * @param tokens An optional list of hex-string contract addresses. If the list
- *        isn't provided, Alchemy will choose based on the top 100 high-volume
- *        tokens on its platform
- */
-export async function getTokenBalances(
-    provider: AlchemyProvider | AlchemyWebSocketProvider,
-    address: HexString,
-    tokens?: string[],
-): Promise<{ contractAddress: string; amount: bigint }[]> {
-    const json: TokenBalances = await provider.send('alchemy_getTokenBalances', [
-        address,
-        tokens || 'DEFAULT_TOKENS',
-    ]);
-
-    // TODO log balances with errors, consider returning an error type
-    return (
-        json.tokenBalances
-            .filter(
-                (
-                    b: TokenBalance,
-                ): b is typeof json['tokenBalances'][0] & {
-                    tokenBalance: Exclude<
-                        typeof json['tokenBalances'][0]['tokenBalance'],
-                        null
-                    >
-                } => b.error === null && b.tokenBalance !== null,
-            )
-            // A hex value of 0x without any subsequent numbers generally means "no
-            // value" (as opposed to 0) in Ethereum implementations, so filter it out
-            // as effectively undefined.
-            .filter(({ tokenBalance }: TokenBalance) => tokenBalance !== '0x')
-            .map((tokenBalance: TokenBalance) => {
-                let balance = tokenBalance.tokenBalance;
-
-                if (balance.length > 66) {
-                    balance = balance.substring(0, 66);
-                }
-                return {
-                    contractAddress: tokenBalance.contractAddress,
-                    amount: BigInt(balance),
-                };
-            })
-    );
 }
 
 /**
