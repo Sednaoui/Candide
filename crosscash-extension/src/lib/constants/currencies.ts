18a433624644df4eed775540f7a3b8114d102cb9
Marc Sednaoui <marcsednaoui@hotmail.com>
2022-02-17 21:02:56 +0100

    add baseAdress to ETH

--- crosscash-extension/src/lib/constants/currencies.ts
+++ crosscash-extension/src/lib/constants/currencies.ts
@@ -1,9 +1,14 @@
 import { FungibleAsset } from '../assets';
 
+// A base address is the zero address for the main asset of the network.
+// For Ethereum, it's ETH / For Polygon, it's MATIC.
+export const baseAddress = '0x0000000000000000000000000000000000000000';
+
 export const ETH: FungibleAsset = {
     name: 'Ether',
     symbol: 'ETH',
     decimals: 18,
+    address: baseAddress,
     metadata: {
         coinGeckoID: 'ethereum',
         logoURL: 'https://i0.wp.com/www.pnglib.com/wp-content/uploads/2020/08/ethereum-purple-blue-icon_5f457c867236d.png?fit=680%2C680&ssl=1',
