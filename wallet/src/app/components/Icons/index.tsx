7094b2f888c1d932d4127fab7762837a4fdb836b
Marc Sednaoui <marcsednaoui@hotmail.com>
2022-06-30 22:47:14 +0200

    add external link icon

---
+++ wallet/src/app/components/Icons/index.tsx
@@ -0,0 +1,4 @@
+import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const externalLinkIcon = <FontAwesomeIcon icon={faExternalLink} />;
