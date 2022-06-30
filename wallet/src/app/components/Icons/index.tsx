1b6d1d42f6a818aa8f181b9edec649dd86172baf
Marc Sednaoui <marcsednaoui@hotmail.com>
2022-06-30 23:07:14 +0200

    add free verified badge as icon

--- wallet/src/app/components/Icons/index.tsx
+++ wallet/src/app/components/Icons/index.tsx
@@ -1,4 +1,8 @@
-import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
+import {
+    faExternalLink,
+    faCertificate,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const externalLinkIcon = <FontAwesomeIcon icon={faExternalLink} />;
+export const verifiedBadgeIcon = <FontAwesomeIcon icon={faCertificate} />;
